41be4b23cd20eae58f73542d9bfd2013fdd6f6f4
Zamer <aelizamer95@gmail.com>
2024-02-09 19:34:12 +0500

    Added the total price , company name and logo

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ for filepath in filepaths:
     pdf.ln(10)
     columns = df.columns
     columns = [item.replace("_", " ").title() for item in columns]
+    # Add header
     pdf.set_font("Times", "B", 10)
     pdf.set_text_color(30, 30, 30)
     pdf.cell(w=30, h=8, txt=columns[0], border=1, align="C")
@@ -31,22 +32,36 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=columns[2], border=1, align="C")
     pdf.cell(w=30, h=8, txt=columns[3], border=1, align="C")
     pdf.cell(w=30, h=8, txt=columns[4], border=1, align="C", ln=1)
-    total = 0
+
+    # Add rows to the table
     for index, row in df.iterrows():
-        total = total + row["total_price"]
-        pdf.set_font("Times", size=10)
+        pdf.set_font("Times", size=12)
         pdf.set_text_color(30, 30, 30)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1, align="C")
         pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1, align="C")
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1, align="C")
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1, align="C")
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, align="C", ln=1)
-    pdf.set_font("Times", size=10)
+
+    # Add total price
+    total_sum = df["total_price"].sum()
+    pdf.set_font("Times", size=12)
     pdf.set_text_color(30, 30, 30)
-    pdf.cell(w=30, h=8, txt=" ", border=1, align="C")
-    pdf.cell(w=70, h=8, txt=" ", border=1, align="C")
-    pdf.cell(w=30, h=8, txt=" ", border=1, align="C")
     pdf.cell(w=30, h=8, txt="", border=1, align="C")
-    pdf.cell(w=30, h=8, txt=str(total), border=1, align="C", ln=1)
+    pdf.cell(w=70, h=8, txt="", border=1, align="C")
+    pdf.cell(w=30, h=8, txt="", border=1, align="C")
+    pdf.cell(w=30, h=8, txt="", border=1, align="C")
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, align="C", ln=1)
+
+    pdf.ln(10)
+
+    # Add total sum sentence
+    pdf.set_font("Times", "B", 17)
+    pdf.cell(w=35, h=10, txt=f"The total price is: {total_sum}", ln=1)
+
+    # Add company name and logo
+    pdf.set_font("Times", "B", 30)
+    pdf.cell(w=54, h=10, txt="PythonHow")
+    pdf.image("pythonhow.png", w=11)
 
     pdf.output(f"PDFs/{filename}.pdf")
