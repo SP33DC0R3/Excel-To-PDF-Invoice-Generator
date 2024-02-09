cbc7c6b7fc28c178de458236fc5061eb7767ca9e
Zamer <aelizamer95@gmail.com>
2024-02-09 19:02:58 +0500

    Added the table

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     pdf = FPDF(orientation="P", unit="mm", format="a4")
     pdf.add_page()
 
@@ -16,9 +14,39 @@ for filepath in filepaths:
     invoice_num, date = filename.split("-")
 
     pdf.set_font("Times", "B", 24)
-    pdf.cell(w=0, h=16, txt=f"Invoice #: {invoice_num}", ln=1)
+    pdf.cell(w=0, h=10, txt=f"Invoice #: {invoice_num}", ln=1)
 
     pdf.set_font("Times", "B", 24)
-    pdf.cell(w=0, h=16, txt=f"Date: {date}", ln=1)
+    pdf.cell(w=0, h=10, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
+    pdf.ln(10)
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font("Times", "B", 10)
+    pdf.set_text_color(30, 30, 30)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1, align="C")
+    pdf.cell(w=70, h=8, txt=columns[1], border=1, align="C")
+    pdf.cell(w=30, h=8, txt=columns[2], border=1, align="C")
+    pdf.cell(w=30, h=8, txt=columns[3], border=1, align="C")
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, align="C", ln=1)
+    total = 0
+    for index, row in df.iterrows():
+        total = total + row["total_price"]
+        pdf.set_font("Times", size=10)
+        pdf.set_text_color(30, 30, 30)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1, align="C")
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1, align="C")
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1, align="C")
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1, align="C")
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, align="C", ln=1)
+    pdf.set_font("Times", size=10)
+    pdf.set_text_color(30, 30, 30)
+    pdf.cell(w=30, h=8, txt=" ", border=1, align="C")
+    pdf.cell(w=70, h=8, txt=" ", border=1, align="C")
+    pdf.cell(w=30, h=8, txt=" ", border=1, align="C")
+    pdf.cell(w=30, h=8, txt="", border=1, align="C")
+    pdf.cell(w=30, h=8, txt=str(total), border=1, align="C", ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
